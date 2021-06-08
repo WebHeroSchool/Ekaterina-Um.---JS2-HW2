6359dc6539ae575aca20fc2be42eeee3dd279bed
shtuchka666 <lyskova@sibmail.com>
2021-06-08 16:42:00 +0300

    new

---
+++ file.js
@@ -0,0 +1,18 @@
+let num;
+let score;
+const ques1 = {
+  question:'Для чего нужна сетевая модель OSI?',
+  answer:'',
+  currectAnswer:'Для стандартизации взаимодействия между сетевыми устройствами'}
+const ques2 = {
+  question:'Сколько уровней содердит модель OSI?',
+  answer:'',
+  currectAnswer:'7'}
+const ques3 = {
+  question:'Сколько уровней содержит стек TCP/IP?',
+  answer:'',
+  currectAnswer:'4'}
+const ques4 = {
+  question:'Какой из перечисленных протоколов работает на транспортном уровне?',
+  answer:'',
+  currectAnswer:'TCP'}
